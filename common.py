4c49fa806600c967ea8d3c611ef300a6fffffd10
Animan8000 <187183497+Veniman8000@users.noreply.github.com>
2026-08-27 18:06:06 +0200

    chore: logo fix

--- common.py
+++ common.py
@@ -16,7 +16,7 @@ from contextlib import contextmanager
 from pathlib import Path, PurePosixPath, PureWindowsPath
 from typing import Any
 
-VERSION = "1.3.1.2"
+VERSION = "1.3.1.3"
 
 def get_tool_dir() -> Path:
     if getattr(sys, "frozen", False):
